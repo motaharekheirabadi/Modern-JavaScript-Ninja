558e09f36e374d001008dd0db58fcf43d463b306
motaharekheirabadi <92826072+motaharekheirabadi@users.noreply.github.com>
2022-07-27 22:32:26 +0430

    chenge file

--- 2-Javascript-Basics/sandbox.js
+++ 2-Javascript-Basics/sandbox.js
@@ -43,14 +43,59 @@ console.log(fullName.length);
 // *string methods
 
 console.log(fullName.toUpperCase());
-let result = fullName.toLocaleLowerCase();
-console.log(result);
+// let result = fullName.toLocaleLowerCase();
+// console.log(result);
 
 let index = email.indexOf('@');
 console.log(index);
 
 let index2 = email.charAt(3);
 console.log(index2);
+
+// *common string methods
+
+// let result1 = email.lastIndexOf("t");
+// let result1 = email.slice(0,6)
+// let result1 = email.substr(0,6)
+// let result1 = email.replace('k' , 'N') 
+let result1 = email.repeat(2)
+console.log(result1);
+
+// !Numbers
+
+let radius = 10;
+const pi =3.14;
+
+// console.log(radius ,pi);
+
+// console.log(10/2);
+// let result = radius % 3;
+// let result = pi * radius**2; 
+
+ /////////////////////////////////////////////
+
+//  let result = 5*(10-3)**2; 
+// console.log(result);
+
+let likes = 10;
+
+// likes = likes + 1;
+// likes++;
+// likes--;
+// likes += 10;
+// likes -= 5
+// likes *= 2
+console.log(likes);
+
+// !NaN = not a number
+console.log(5 / 'hello');
+
+
+
+
+
+
+
 
 
 
